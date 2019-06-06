93a6f7bf21957d41db7dc7b19e1482c523e556ad
SnackKing <allegretti813@gmail.com>
2019-06-06 12:52:26 -0400

    Added search bar in action bar

--- app/src/main/java/com/example/alleg/tradetester/HomeActivity.kt
+++ app/src/main/java/com/example/alleg/tradetester/HomeActivity.kt
@@ -1,11 +1,18 @@
 package com.example.alleg.tradetester
 
+import android.app.SearchManager
+import android.content.Context
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import com.example.alleg.tradetester.dummy.DummyContent
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
+import android.view.MenuInflater
+import android.support.v4.view.MenuItemCompat.getActionView
+import android.widget.SearchView
+
 
 class HomeActivity : StockListFragment.OnListFragmentInteractionListener, WatchedFragment.OnListFragmentInteractionListener, AppCompatActivity() {
 
@@ -23,6 +30,18 @@ class HomeActivity : StockListFragment.OnListFragmentInteractionListener, Watche
         auth = FirebaseAuth.getInstance()
         database = FirebaseDatabase.getInstance().reference
         setContentView(R.layout.activity_home)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.options_menu, menu)
+        // Associate searchable configuration with the SearchView
+        val searchManager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+        val searchView = menu.findItem(R.id.search).actionView as SearchView
+        searchView.setSearchableInfo(
+                searchManager.getSearchableInfo(componentName))
+
+        return true
     }
 
 }
