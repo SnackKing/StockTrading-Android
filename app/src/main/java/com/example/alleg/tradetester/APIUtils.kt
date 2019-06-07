729abe830e15f061fb04ed1ba8c881c5b47d08d0
SnackKing <allegretti813@gmail.com>
2019-06-07 14:30:25 -0400

    Added API call for getting historical chart data. Chart library now takes in stock data and displays correct price graph

--- app/src/main/java/com/example/alleg/tradetester/APIUtils.kt
+++ app/src/main/java/com/example/alleg/tradetester/APIUtils.kt
@@ -3,12 +3,21 @@ package com.example.alleg.tradetester
 import android.content.Context
 import android.content.Intent
 import android.support.v4.content.ContextCompat.startActivity
+import android.util.Log
 import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
 import org.json.JSONArray
 import org.json.JSONObject
+import java.text.SimpleDateFormat
+import java.util.*
+
+
+
+
+
+
 object APIUtils{
     public fun makeAPICallForSingleStock(symbol: String, context: Context) {
         val queue = Volley.newRequestQueue(context)
@@ -43,5 +52,54 @@ object APIUtils{
         queue.add(jsonObjectRequest)
 
 
+    }
+
+    class HistoryResponse(symbol: String, context: Context){
+        var labels = arrayListOf<String>()
+         var prices = arrayListOf<Float>()
+        var ready = false;
+        init{
+            val queue = Volley.newRequestQueue(context)
+            val format = SimpleDateFormat("yyyy-MM-dd")
+            val today = format.format(Date())
+            val weekAgo = getEarlierDate(7, format)
+            var url = "https://www.worldtradingdata.com/api/v1/history?"
+            url += "api_token=mkUwgwc7TADeShHuZO7D2RRbeLu1b9PNd6Ptey0LkIeRliCUjdLJJB9UE4UX"
+            url += "&symbol=" + symbol
+            url += "&date_from=" + weekAgo
+            url += "&date_to=" + today
+            url += "&sort=oldest"
+            url += "&output=json"
+            val jsonObjectRequest = JsonObjectRequest(Request.Method.GET, url, null,
+                    Response.Listener { response ->
+                            var data = response.getJSONObject("history")
+                            val keys = data.names()
+                            for (i in 0 until keys.length()) {
+                                val key = keys.getString(i)
+                                labels.add(key)
+                                prices.add(data.getJSONObject(key).getString("close").toFloat())
+                            }
+                            ready = true;
+                        val act = context as StockActivity
+                        act.setupLineChart(this)
+
+
+                    },
+
+                    Response.ErrorListener { error ->
+                        // TODO: Handle error
+                        Log.d("UTIL", error.toString())
+                    }
+            )
+            queue.add(jsonObjectRequest)
+
+        }
+        private fun getEarlierDate(numDays:Int, format:SimpleDateFormat):String{
+            val cal = Calendar.getInstance()
+            cal.time = Date()
+            cal.add(Calendar.DATE, numDays*-1)
+            val dateBefore = cal.time
+            return format.format(dateBefore)
+        }
     }
 }
--- app/src/main/java/com/example/alleg/tradetester/StockActivity.kt
+++ app/src/main/java/com/example/alleg/tradetester/StockActivity.kt
@@ -2,28 +2,60 @@ package com.example.alleg.tradetester
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import org.json.JSONArray
 import org.json.JSONObject
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData
-import com.github.mikephil.charting.data.LineDataSet
 import kotlinx.android.synthetic.main.activity_stock.*
+import com.github.mikephil.charting.components.AxisBase
+import com.github.mikephil.charting.data.LineDataSet
+import com.github.mikephil.charting.formatter.IValueFormatter
+import com.github.mikephil.charting.utils.ViewPortHandler
+import com.github.mikephil.charting.formatter.IAxisValueFormatter;
+import java.text.DecimalFormat
 
 
 class StockActivity : AppCompatActivity() {
-
+    private lateinit var sym:String
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_stock)
         val mJsonObject = JSONObject(intent.getStringExtra("data"))
-        symbol.text = mJsonObject.get("symbol").toString()
+        sym =  mJsonObject.get("symbol").toString()
+        symbol.text = sym
+
+        Log.d("STOCK", "THIS WORKED")
+        APIUtils.HistoryResponse(sym,this)
+
+
+
+
+    }
+    fun setupLineChart(historyData:APIUtils.HistoryResponse){
+
         val entries = ArrayList<Entry>()
-        for(i in 1..7){
-            entries.add(Entry(i.toFloat(),Math.random().toFloat()))
+        for(i in 0 until 5){
+            entries.add(Entry(i.toFloat(),historyData.prices[i]))
         }
         val linedata:LineDataSet = LineDataSet(entries, "Test Data")
         chart.setData(LineData(linedata))
+        chart.xAxis.granularity = 1f
+        chart.xAxis.setValueFormatter(MyValueFormatter(historyData.labels))
+        chart.invalidate()
+    }
 
+    inner class MyValueFormatter(val dates:ArrayList<String>) : IAxisValueFormatter {
+
+
+        private val mFormat: DecimalFormat
+
+        init {
+            mFormat = DecimalFormat("###,###,##0") // use one decimal
+        }
+        override fun getFormattedValue(value: Float, axis: AxisBase?): String {
+            return dates[value.toInt()]
+        }
 
     }
 }
