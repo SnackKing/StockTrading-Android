1e8ff5d638fb574db7fb37b0a1027be650b20a5c
SnackKing <allegretti813@gmail.com>
2019-06-13 20:27:35 -0400

    Added frontend for stock news

--- app/src/main/java/com/example/alleg/tradetester/NewsListAdapter.kt
+++ app/src/main/java/com/example/alleg/tradetester/NewsListAdapter.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.BaseAdapter
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.news_item.view.*
 
 class NewsListAdapter(private val context: Context, private val items:ArrayList<NewsItem>): BaseAdapter() {
@@ -26,6 +27,19 @@ class NewsListAdapter(private val context: Context, private val items:ArrayList<
         val rowView = inflater.inflate(R.layout.news_item, parent, false)
         val curItem: NewsItem = getItem(position) as NewsItem
         rowView.title.text = curItem.title
+        Picasso.get().load(curItem.imageUrl).into(rowView.image)
+        rowView.source.text = curItem.sourceName
+        rowView.content.text = curItem.text
+        rowView.date.text = curItem.date
+        rowView.sentiment.text = curItem.sentiment
+        if(curItem.sentiment == "Positive"){
+            rowView.sentiment.setTextColor(context.resources.getColor(R.color.green))
+        }
+        else if(curItem.sentiment == "Negative"){
+            rowView.sentiment.setTextColor(context.resources.getColor(R.color.red))
+        }
+
+
         return rowView
     }
 }
