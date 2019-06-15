dbcecffa5d99024984e66ad0f05e9d900ef37282
SnackKing <allegretti813@gmail.com>
2019-06-14 20:43:04 -0400

    Added overflow menu to main page

--- app/src/main/java/com/example/alleg/tradetester/HomeActivity.kt
+++ app/src/main/java/com/example/alleg/tradetester/HomeActivity.kt
@@ -5,7 +5,9 @@ import android.content.Context
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
+import android.view.MenuItem
 import android.widget.SearchView
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
@@ -41,6 +43,45 @@ class HomeActivity : StockListFragment.OnListFragmentInteractionListener, Watche
                 searchManager.getSearchableInfo(componentName))
 
         return true
+    }
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+        R.id.search -> {
+            // User chose the "Settings" item, show the app settings UI...
+            Toast.makeText(this, "Search", Toast.LENGTH_SHORT).show()
+            true
+        }
+
+        R.id.account -> {
+            // User chose the "Favorite" action, mark the current item
+            // as a favorite...
+            Toast.makeText(this, "account", Toast.LENGTH_SHORT).show()
+
+            true
+        }
+        R.id.transactions -> {
+            // User chose the "Favorite" action, mark the current item
+            // as a favorite...
+            Toast.makeText(this, "trans", Toast.LENGTH_SHORT).show()
+
+            true
+        }
+        R.id.about -> {
+            // User chose the "Favorite" action, mark the current item
+            // as a favorite...
+            Toast.makeText(this, "about", Toast.LENGTH_SHORT).show()
+            true
+        }
+        R.id.signout -> {
+            // User chose the "Favorite" action, mark the current item
+            // as a favorite...
+            Toast.makeText(this, "signout", Toast.LENGTH_SHORT).show()
+            true
+        }
+        else -> {
+            // If we got here, the user's action was not recognized.
+            // Invoke the superclass to handle it.
+            super.onOptionsItemSelected(item)
+        }
     }
 
 }
